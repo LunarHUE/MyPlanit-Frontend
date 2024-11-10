35817bb28ac71bf05ad5d50c20a369c0c2c64e92
KickedSoda <96560330+KickedSoda@users.noreply.github.com>
2024-11-10 10:26:00 -0600

    Delete profile action

--- components/sidebar/data.tsx
+++ components/sidebar/data.tsx
@@ -1,51 +1,55 @@
 // Careful what you import here, the default import of this file is the React component
-import { type SidebarItem } from "./sidebar-item";
-import { type DropDownItem } from "./dropdown-item";
-import { BookText, Calendar, House, Inbox } from 'lucide-react';
+import { type SidebarItem } from './sidebar-item'
+import { type DropDownItem } from './dropdown-item'
+import { BookText, Calendar, House, Inbox } from 'lucide-react'
+import SendAuthedRequest from '@/app/_actions/send-authed-api'
 
-export  const sideBarItems: SidebarItem[] = [
+export const sideBarItems: SidebarItem[] = [
   {
     icon: <House />,
-    id: "home",
+    id: 'home',
     content: <span>Home</span>,
-    href: "/"
+    href: '/',
   },
   {
     icon: <BookText />,
-    id: "courses",
+    id: 'courses',
   },
   {
-    icon: <Calendar/>,
-    id: "calendar",
+    icon: <Calendar />,
+    id: 'calendar',
     content: <span>Calendar</span>,
-    href: "/calendar",
+    href: '/calendar',
   },
   {
-    icon: <Inbox/>,
-    id: "inbox",
+    icon: <Inbox />,
+    id: 'inbox',
     content: <span>Inbox</span>,
-    href: "/inbox",
+    href: '/inbox',
   },
-];
+]
 
 export const dropDownItems: DropDownItem[] = [
   {
-    id: "profile",
-    action: "link",
-    content: <span>Profile</span>,
-    href: "#profile",
-  },
-  {
-    id: "settings",
-    action: "link",
-    content: <span>Settings</span>,
-    href: "#settings",
-  },
-  {
-    id: "logout",
+    id: 'delete',
     action: () => {
-      console.log("logout");
+      SendAuthedRequest('/profile', 'DELETE').then(() => {
+        window.location.href = '/api/auth/logout'
+      })
+    },
+    content: <span>Delete Profile</span>,
+  },
+  {
+    id: 'settings',
+    action: 'link',
+    content: <span>Settings</span>,
+    href: '#settings',
+  },
+  {
+    id: 'logout',
+    action: () => {
+      window.location.href = '/api/auth/logout'
     },
     content: <span>Logout</span>,
-  }
+  },
 ]
